9a466d706cef0c58cd5f40bf348c37b74e931c5b
Ritesh Omen <riteschougule96@gmail.com>
2024-08-05 14:38:40 +0530

    Updated Code

---
+++ Java/String/SmallestLargestNo.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class SmallestLargestNo {
+    public static void main(String[] args) {
+        String str = "Ritesh Ramprakash Chougule Tardal";
+        SmallAndLagge(str);
+        
+    }  
+    public static void SmallAndLagge(String str){
+        String s[] = str.split(" ");
+        int max = 0;
+        String result="";
+        for (int i = 0; i < s.length; i++) {
+            String a = s[i];
+             int count = a.length();
+             if (count>max){
+                max=count;
+                result = s[i];
+             }
+        }
+
+        //max =13
+        int min =max;
+        String minResult="";
+        for (int i = 0; i < s.length; i++) {
+            int count1 = s[i].length();
+
+            if (min>=count1) {
+                min = count1;
+                minResult = s[i];
+
+            }
+
+        }
+        System.err.println(result);
+        System.err.println(minResult);
+}
+}
