0f6d0dd757f1335458245cae4f5d94a5de6a3dc1
Ritesh Omen <riteschougule96@gmail.com>
2024-08-06 11:28:54 +0530

    New File

--- Java/Array/FindElement.java
+++ Java/Array/FindElement.java
@@ -2,6 +2,18 @@ package RiteshRC96.Java.Array;
 
 public class FindElement {
     public static void main(String[] args) {
+        int arr[] = {1,2,3,5,6};
+        System.out.println(Check(arr, 0));
+        System.out.println(Check(arr, 3));
         
+    }
+
+    public static boolean Check(int arr[], int n){    
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) {
+                return true;
+            }
+        }
+        return false;
     }
 }
