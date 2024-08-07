5310cc9e9a645599d4292ef8ac58187006e27f7e
Ritesh Omen <riteschougule96@gmail.com>
2024-08-07 20:00:36 +0530

    New File

---
+++ Java/Numbers/SumOfDivisor.java
@@ -0,0 +1,20 @@
+package RiteshRC96.Java.Numbers;
+
+public class SumOfDivisor {
+    public static void main(String[] args) {
+        System.out.println(Sum(6));
+        //1+2+3+6 = 12
+        
+    }
+
+    public static int Sum(int num){
+        int sum =0;
+        for (int i = 1; i <=num; i++) {
+            if (num%i ==0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+
+}
