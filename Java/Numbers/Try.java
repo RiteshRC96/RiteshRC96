7e1756901f609d81b68f059f1e3ab64f3c7a6a12
Ritesh Omen <riteschougule96@gmail.com>
2024-08-09 15:39:37 +0530

    Updated file

--- Java/Numbers/Try.java
+++ Java/Numbers/Try.java
@@ -2,12 +2,25 @@ package RiteshRC96.Java.Numbers;
 
 public class Try {
     public static void main(String[] args) {
-        int number =16;
-        int count = 0;
-        while (number != 0 && number%2 ==0) {
-            number = number/2;
-            count++;
+        
+        int arr[] = {11,21,35,48,60,12,72};
+        check(arr, 2);
+    }
+    public static void check(int arr[], int k) {
+
+        
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            int sum=0;
+            while (temp>0) {
+                int rem = temp%10;
+                sum = sum + rem;
+                temp = temp/10;
+            }
+            
+                if (sum%k ==0 && arr[i] % k != 0) {
+                System.out.println(arr[i]);
+            }
         }
-        System.out.println(count);
     }
 }
