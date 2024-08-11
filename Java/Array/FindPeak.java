08ee9a27737c9b36d27310b164e7f78aebcab811
Ritesh Omen <riteschougule96@gmail.com>
2024-08-11 17:51:20 +0530

    Updated file

--- Java/Array/FindPeak.java
+++ Java/Array/FindPeak.java
@@ -6,7 +6,7 @@ public class FindPeak {
     public static void main(String[] args) {
 
         int arr[] = {1,2,3,4,3,2,1};
-        int arr1[] = {1,2,3,4,3,2,1,5,6,7,8,9,8,7,6,5};
+        //int arr1[] = {1,2,3,4,3,2,1,5,6,7,8,9,8,7,6,5};
         peak(arr);
         //peak(arr1);
 
